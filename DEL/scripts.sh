26acff152175c3752739eb37c7d84a032d92fe24
veronika <vopengame@gmail.com>
2025-02-03 18:03:25 +0300

    Update scripts.sh

--- DEL/scripts.sh
+++ DEL/scripts.sh
@@ -26,3 +26,28 @@ fi
 
 if [ $1 -eq "8" ]; then docker exec -it $(docker ps -aq) cat /etc/nginx/nginx.conf
 fi
+
+--Ex 04 -- 
+docker stop $(docker ps -q)         # остановить запущенные контейнеры
+docker rm -f $(docker ps -aq)       # Убираю все контейнеры 
+docker rmi -f $(docker images -q)   # Убираю все образы
+docker pull nginx                   # Скачиваю новый nginx 
+docker run -d -p 80:81 nginx        # запускаю с мапингом портов
+docker cp server $(docker ps -q):/  # переносим сервер в контейнер
+docker exec $(docker ps -q) ls      # Проверяем что он появился 
+
+# Установить внутрь контейнера spqwn-fcgi и все для компиляции сервера 
+# Для начала обновимся 
+docker exec $(docker ps -q) apt-get update 
+docker exec $(docker ps -q) apt-get install -y spawn-fcgi  libfcgi-dev gcc
+# можно попробовать через одну команду это сделать docker exec $(docker ps -q) apt-get update && apt-get install -y spawn-fcgi  libfcgi-dev gcc
+
+ docker exec $(docker ps -q) gcc -o server server.c -lfcgi  # компилируем файл server.c
+ # изменяем файл конфига
+docker cp nginx.config $(docker ps -q):/etc/nginx/          # переносим файл конфига в контейнер 
+docker exec $(docker ps -q) nginx -s reload                 # перезапускаем 
+# Если во время перезапуска возникнет ошибка с PID попробовать полностью рестертануть docker exec $(docker ps -q) service nginx start
+
+docker exec $(docker ps -q) spawn-fcgi -p 8080 server      # запускаею на порту 8080 получаю ошибку 127, Error 2 говорит о том что файла нет 
+
+--Ex 04  -- END -- 
