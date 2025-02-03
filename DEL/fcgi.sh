85cde310dc719c3a95443dca26c8b8d8cdaaa5ab
veronika <vopengame@gmail.com>
2025-02-03 17:41:25 +0300

    Update fcgi.sh

--- DEL/fcgi.sh
+++ DEL/fcgi.sh
@@ -3,10 +3,7 @@ fi
 if [ $(docker ps -aq) <> '' ]; then  docker rm -f $(docker ps -aq)
 fi
 
-
 printf '\033[01;32m docker run -d -p 80:81 nginx>>\033[00m\n'
-
-
 docker run -d -p 80:81 nginx
 
 printf '\033[01;32m update >>\033[00m\n'
@@ -15,15 +12,18 @@ docker exec $(docker ps -q) apt-get -y install gcc
 docker exec $(docker ps -q) apt-get -y install libfcgi-dev
 docker exec $(docker ps -q) apt-get -y install spawn-fcgi
 
-#curl http://localhost:80
 
+printf '\033[01;32m --- copy file in conteiner --- \033[00m\n'
+docker cp server.c $(docker ps -q):/
+docker cp nginx.conf $(docker ps -q):/etc/nginx/
 
 
 echo "--- --- reload nginx --- ---"
 docker exec $(docker ps -q) nginx -s reload
-gcc -o server server.c -lfcgi
+docker exec $(docker ps -q) gcc -o server server.c -lfcgi
 docker exec $(docker ps -q) spawn-fcgi -p 8080 server
-#./server
 
 docker exec $(docker ps -q)  curl http://127.0.0.1:81
 curl http://localhost:80
+
+#ps aux --- ??? ---
