41da7f93e866f9891b3fc096a23d177d710512bf
veronika <vopengame@gmail.com>
2025-02-14 14:12:43 +0300

    Update p3.sh

--- SimplDocker/part3/p3.sh
+++ SimplDocker/part3/p3.sh
@@ -3,20 +3,17 @@
 ### ### ### ### ### ### ### ### ### 
 
 if [ $1 -eq "30" ]; then  # install docker server
-	printf '\033[01;32m docker run -d -p 80:81 nginx>>\033[00m\n'
+
 docker run -d -p 80:81 nginx
 
-	printf '\033[01;32m update >>\033[00m\n'
-docker exec $(docker ps -q) apt-get update > ./log/update
-	printf '\033[01;32m install gcc >>\033[00m\n'
+
+docker exec $(docker ps -q) apt-get update > ./log/update  # ход операции выводм в файл а не в консоль
 docker exec $(docker ps -q) apt-get -y install gcc > ./log/gcc
-	printf '\033[01;32m install libfcgi-dev >>\033[00m\n'
 docker exec $(docker ps -q) apt-get -y install libfcgi-dev > ./log/libfcgi-dev
-	printf '\033[01;32m install install spawn-fcgi >>\033[00m\n'
 docker exec $(docker ps -q) apt-get -y install spawn-fcgi > ./log/spawn-fcgi
 
 	printf '\033[01;32m --- copy file in conteiner server.c  && nginx.conf >>--- \033[00m\n'
-docker cp ~/part3/server.c $(docker ps -q):/
+docker cp ~/part3/server.c $(docker ps -q):/  # ~/  означчает домашнюю дирректорию
 docker cp ~/part3/nginx/nginx.conf $(docker ps -q):/etc/nginx/
 docker exec $(docker ps -q) ls      # Проверяем что он появился
 
