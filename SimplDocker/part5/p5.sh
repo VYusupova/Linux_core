21c486780210936139a268968708c55403a8acd7
veronika <vopengame@gmail.com>
2025-02-14 15:29:53 +0300

    Update p5.sh

--- SimplDocker/part5/p5.sh
+++ SimplDocker/part5/p5.sh
@@ -9,12 +9,12 @@ docker build -t nginx:ex05 .
 	printf '\n\t\t\t\033[01;34m <<<   BILD NEW image   >>>\n\t\t '
 docker images
 docker run -d -p 80:81 nginx:ex05
-	printf '\t\t\t\033[01;32m <<<   CHEK you conteiner is allive  >>>\033[00m\n\t\t ---   --- \n' 
+	printf '\t\t\t\033[01;32m <<<   CHECK conteiner is alive  >>>\033[00m\n\t\t ---   --- \n' 
 	docker ps
-	printf '\n\t\t\t\033[01;31m <<<   CHEK dockle you image  >>>\033[00m\n\t\t ---   --- \n' 
+	printf '\n\t\t\t\033[01;31m <<<   CHECK dockle you image  >>>\033[00m\n\t\t ---   --- \n' 
 export DOCKER_CONTENT_TRUST=1  # need to write CIS-DI-0005
 dockle --ignore CIS-DI-0010 nginx:ex05
-	printf '\n\t\t\t\033[01;32m <<<   LOOK what server is worked  >>>\033[00m\n\t\t  \n' 
+	printf '\n\t\t\t\033[01;32m <<<  let's see that the server is running  >>>\033[00m\n\t\t  \n' 
 curl localhost:80
 curl localhost/status
 fi
